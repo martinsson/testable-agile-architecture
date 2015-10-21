606d66034ecc9b823a146a0e56a3b1682fec6a8a
Johan Martinsson <martinsson.johan@gmail.com>
2015-10-21 17:48:43 +0200

    simplified to minimum, no promises no callbacks

--- src/LangPostService.js
+++ src/LangPostService.js
@@ -1,17 +1,9 @@
-/**
- * Created by johan on 26/06/15.
- * Update by f.marisa on 06/07/15.
- */
 var _ = require('lodash');
-var fs = require('fs-extra-promise');
-var path = require('path');
-var pdfUtility = require('beevirtua-pdf-utility');
-var Promise = require('bluebird');
+var pdfUtility = require('pdf-utility');
 var nodeUuid = require('node-uuid');
 
-var FaceService = require('./face/FaceService');
+var Face = require('./face/Face');
 
-var debug = require('debug')('LangPostService');
 
 function LangPostService(jobQueue, fileSystemContext) {
     this.queue = jobQueue;
@@ -23,41 +15,29 @@ function LangPostService(jobQueue, fileSystemContext) {
 }
 
 LangPostService.prototype.postLang = function (parentEntityKey, pdfAbsolutePath, forcedLangId) {
-    var self = this;
+    var pdfInfo = pdfUtility.getPdfInfo(pdfAbsolutePath);
+    var langId = defineLang(forcedLangId, pdfInfo);
+    var faces = buildFaces(pdfInfo);
+
     var lang = {
-        id: undefined,
-        displayMode: 'biplan',
-        face: []
-
+        id: langId,
+        displayMode: 'portrait',
+        face: faces
     };
-    return pdfUtility.getPdfInfo(pdfAbsolutePath).then(function (pdfInfo) {
-        lang.id = defineLang(forcedLangId, pdfInfo);
 
-        lang.face = buildFaces(pdfInfo);
+    var langEntityKey = parentEntityKey.append('lang', langId);
+    var jobPayload = buildDataForSplitJob(langEntityKey, lang, pdfAbsolutePath);
 
-        var jobsPayload = buildDataForSplitJob(parentEntityKey, lang, pdfAbsolutePath);
 
-        return Promise.map(jobsPayload, function (jobPayload) {
-            return new Promise(function (resolve, reject) {
-                var job = self.queue.create(self.splitJobConfig.queueName, jobPayload)
-                    .attempts(self.splitJobConfig.attempts)
-                    .save(function (err) {
-                        if (!err) {
-                            resolve(job.id);
-                        } else {
-                            reject(err);
-                        }
-                    });
-            });
-        })
-    });
+    this.queue.create(this.splitJobConfig.queueName, jobPayload)
+        .attempts(this.splitJobConfig.attempts)
+        .save();
 };
 
 
 function buildFaces(pdfInfo) {
     return _.times(pdfInfo.numberOfPages, function () {
-        var faceService = new FaceService(repository);
-        return faceService.buildFace({
+        return Face.buildFace({
             id: nodeUuid.v1(),
             dimensions: {
                 width: pdfInfo.width,
@@ -66,6 +46,7 @@ function buildFaces(pdfInfo) {
         });
     });
 }
+
 function defineLang(forcedLangId, pdfInfo) {
     var langId;
     if (forcedLangId) {
@@ -77,20 +58,19 @@ function defineLang(forcedLangId, pdfInfo) {
     }
     return langId;
 }
-function buildDataForSplitJob(parentEntityKey, lang, pdfAbsolutePath) {
-    var langEntityKey = parentEntityKey.append('lang', lang.id);
-    var faces = lang.face;
+
+function buildDataForSplitJob(langEntityKey, faces, pdfAbsolutePath) {
     var faceRoutes = faces.map(function (face) {
         return [langEntityKey.directory(), 'face', face.id].join('/');
     });
     var bvIds = langEntityKey.directory().split('/');
     var jobTitle = [bvIds[2], bvIds[4], bvIds[6]].join('_');
-    var jobsPayload = [{
+    return {
         title: jobTitle,
         originalFilepath: pdfAbsolutePath,
         faceRoutes: faceRoutes,
-        startSplit: 1,
-    }];
-    return jobsPayload;
+        startSplit: 1
+    };
 }
+
 module.exports = LangPostService;
