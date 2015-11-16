c860047559ef5dd3d9fd0999e7f2ce6be1e9d7c1
Johan Martinsson <martinsson.johan@gmail.com>
2015-11-16 13:19:27 +0100

    clearer version of MyQueue

--- src/MyQueue.js
+++ src/MyQueue.js
@@ -4,13 +4,15 @@
  * @returns {{submit: submit}}
  */
 function myQueue(jobQueue, splitJobConfig) {
-    return {
-        submit: function(jobPayload) {
-            jobQueue.create(splitJobConfig.queueName, jobPayload)
-                .attempts(splitJobConfig.attempts)
-                .save();
 
-        }
+    function submit(jobPayload) {
+        jobQueue.create(splitJobConfig.queueName, jobPayload)
+            .attempts(splitJobConfig.attempts)
+            .save();
+    }
+
+    return {
+        submit: submit
     }
 }
 
