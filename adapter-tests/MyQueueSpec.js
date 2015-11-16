62d875938e481af6d43ce2c260d36d46312d7311
Johan Martinsson <martinsson.johan@gmail.com>
2015-11-16 23:27:53 +0100

    made adapter test work

--- adapter-tests/MyQueueSpec.js
+++ adapter-tests/MyQueueSpec.js
@@ -9,99 +9,120 @@ function failure(errorMessage) {
 }
 
 
+function jobConfig(queueName) {
+    return {queueName: queueName, attempts: 1};
+}
+
 describe('submit', function () {
-    var redisConfig = redisConf();
+    describe('normally should', function () {
 
-
-     it('saves the payload to queue, the receiver gets the exact same data', function (done) {
-        var queueName = "test1";
-        var myQueue = MyQueue(redisConfig, queueName);
+        var redisConfig = redisConf();
         var receiver = kue.createQueue(redisConfig);
 
-        var jobData = {id: "jobId1"};
+        it('saves the payload to queue, the receiver gets the exact same data', function (done) {
+            var queueName = "test1";
+            var myQueue = MyQueue(redisConfig, jobConfig(queueName));
 
-        //When
-        myQueue.submit([jobData], queueName);
+            var jobData = {id: "jobId1"};
 
-        //Then
-        receiver.process(queueName, function (job) {
-            expect(job.data).to.deep.equal(jobData)
-            done();
-        });
-    });
+            //When
+            myQueue.submit(jobData, queueName);
 
-    it('only sees events of a single queuname', function (done) {
-
-        var queueWithData = "queueWithData";
-        var myQueue = MyQueue(redisConfig, queueName);
-        var receiver = kue.createQueue(redisConfig);
-
-
-        //When
-        var jobData = {};
-        myQueue.submit([jobData], queueWithData);
-
-        //Then
-        receiver.process("queueWithNothingInIt", function (job) {
-            done(failure("shouldn't have gotten here"))
+            //Then
+            receiver.process(queueName, function (job) {
+                expect(job.data).to.deep.equal(jobData)
+                done();
+            });
         });
 
-        receiver.process(queueWithData, function (job) {
-            done();
+        it('only sees events of a single queuname', function (done) {
+
+            var queueWithData = "queueWithData";
+            var myQueue = MyQueue(redisConfig, jobConfig(queueWithData));
+
+
+            //When
+            var jobData = {};
+            myQueue.submit([jobData], queueWithData);
+
+            //Then
+            receiver.process("queueWithNothingInIt", function (job) {
+                done(failure("shouldn't have gotten here"))
+            });
+
+            receiver.process(queueWithData, function (job) {
+                done();
+            });
+
         });
 
-    });
+        it('it is fifo', function (done) {
+            var queueName = "fifo";
+            var myQueue = MyQueue(redisConfig, jobConfig(queueName));
 
-    it('it is fifo', function (done) {
-        var queueName = "fifo";
-        var myQueue = MyQueue(redisConfig, queueName);
+            var firstJobData = {id: "job1"};
+            var secondJobData = {id: "job2"};
 
-        var firstJobData = {id: "job1"};
-        var secondJobData = {id: "job2"};
+            //When
+            var submittedJobs = [firstJobData, secondJobData];
 
-        //When
-        var submittedJobs = [firstJobData, secondJobData];
+            myQueue.submit(firstJobData);
+            myQueue.submit(secondJobData);
 
-        myQueue.submit(submittedJobs, queueName);
+            var expectedJobNumber = submittedJobs.length;
+            waitForJobsToBeProcessed(expectedJobNumber, queueName)
+                .then(assertJobsCameInOrder)
+                .then(done);
+
+            function assertJobsCameInOrder(receivedJobs) {
+                expect(receivedJobs).to.deep.equal(submittedJobs);
+            }
+
+            //Then
+
+        });
+
+        it('does not matter if the reciever is started before or after the submitter', function () {
+
+        });
 
 
-        var expectedJobNumber = submittedJobs.length;
-        waitForJobsToBeProcessed(expectedJobNumber, queueName)
-            .then(assertJobsCameInOrder)
-            .then(done);
+        function waitForJobsToBeProcessed(expectedJobNumber, queueName) {
 
-        function assertJobsCameInOrder(receivedJobs) {
-            expect(receivedJobs).to.deep.equal(submittedJobs);
+            return new Promise(function (resolve) {
+                var receivedJobs = [];
+                setInterval(receiveOneJob, 5);
+
+                function receiveOneJob() {
+                    receiver.process(queueName, doOneJob);
+                }
+
+                function doOneJob(job) {
+                    receivedJobs.push(job.data);
+                    if (receivedJobs.length === expectedJobNumber) {
+                        resolve(receivedJobs);
+                    }
+                }
+            });
+
         }
-
-        //Then
-
     });
 
-    it('does not matter if the reciever is started before or after the submitter', function () {
-
-    });
-
-    describe('upon failure', function() {
-
-        it.skip('tries as many times as is configured', function (done) {
-            var host = "192.1.99.100";
-            var bogusPort = "3000000";
-
-            var redisConfig  = {
-                redis: "redis://" + host + ":" + bogusPort,
-                attempts: 1
+    describe('upon failure should', function () {
+        this.timeout(3000)
+        it.skip('try as many times as is configured', function (done) {
+            var bogusRedisConfig = {
+                redis: "redis://192.1.99.99:3333"
             };
 
             var queueName = "test attemps";
-            var myQueue = MyQueue(redisConfig, queueName);
-            var receiver = kue.createQueue(redisConfig);
-
-
+            var myQueue = MyQueue(bogusRedisConfig, {queueName: queueName, attempts: 9});
             var jobData = {id: "job that never should be received"};
 
             //When
-            myQueue.submit([jobData], queueName);
+            myQueue.submit(jobData);
+
+            var receiver = kue.createQueue(bogusRedisConfig);
 
             receiver.process(queueName, function (job) {
                 console.log(job)
@@ -110,31 +131,8 @@ describe('submit', function () {
 
         });
 
-        it('tries once every second', function () {
-
-        });
     });
 
-
-
-    function waitForJobsToBeProcessed(expectedJobNumber, queueName) {
-        return new Promise(function (resolve) {
-            var receivedJobs = [];
-            setInterval(receiveOneJob, 5);
-
-            function receiveOneJob() {
-                receiver.process(queueName, doOneJob);
-            }
-
-            function doOneJob(job) {
-                receivedJobs.push(job.data);
-                if (receivedJobs.length === expectedJobNumber) {
-                    resolve(receivedJobs);
-                }
-            }
-        });
-
-    }
 });
 
 
